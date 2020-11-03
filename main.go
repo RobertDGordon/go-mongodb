4da4dcdd0a7563a2b26db8326cd5bc948572e88f
Robert Gordon <Komeuppance@gmail.com>
2020-11-02 22:28:14 -0800

    Success, connected to Atlas

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+//LoadEnv loads variables from .env
+func LoadEnv(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func main() {
+	fmt.Println("Starting server...")
+
+	username := LoadEnv("USER")
+	password := LoadEnv("PASSWORD")
+	dbname := LoadEnv("DBNAME")
+
+	fmt.Printf("Username: %s \n", username)
+	fmt.Printf("Connecting to: %s \n", dbname)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	if cancel != nil {
+		fmt.Println("Ctx:", cancel)
+		// log.Fatal(cancel) //Do not error out
+	}
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://"+username+":"+password+"@cluster0.ing9e.mongodb.net/"+dbname+"?retryWrites=true&w=majority"))
+	err = client.Connect(ctx)
+	if err != nil {
+		fmt.Println("client.Connect:", err)
+		// log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		fmt.Println("Ping error")
+		log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		fmt.Println("Database error")
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
+}
